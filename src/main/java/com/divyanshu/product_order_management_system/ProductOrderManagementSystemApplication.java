4309207b7d314c32c5b2200eb87cf99dbdbacaa2
divyanshu <divyanshupunse3@gmail.com>
2026-01-16 21:37:35 +0530

    Initial commit

---
+++ src/main/java/com/divyanshu/product_order_management_system/ProductOrderManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.divyanshu.product_order_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrderManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/divyanshu/product_order_management_system/ProductOrderManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyanshu.product_order_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
